5dcb6725b70ecb6d5a7e2e814bc0f6405ca5d5c4
TheFlyyingPigs <theflyingpigs10@gmail.com>
2026-07-18 09:56:46 -0500

    poison stacks
    -changed timer so that it is capable of increasing time while running

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,7 +5,6 @@ const SPEED = 10000
 var target_player_hp := 150.0
 
 @onready var poison_timer: Timer = %PoisonTimer
-@onready var poison_tick_timer: Timer = %PoisonTickTimer
 @onready var hp_label: Label = $HpLabel
 
 func _ready() -> void:
@@ -25,19 +24,16 @@ func _process(_delta: float) -> void:
 	"+str(Globals.player_hp)+ "/150.0"
 
 func poisoned():
-	Globals.player_poi = true
-	poison_timer.wait_time += Globals.on_hit_poi
+	Globals.is_player_poi = true
+	Globals.player_poi += Globals.on_hit_poi
+	
 	if poison_timer.is_stopped():
 		poison_timer.start()
-	if poison_tick_timer.is_stopped():
-		poison_tick_timer.start()
+	print("poison is "+str(Globals.player_poi))
 
 func poison_tick():
-	if Globals.player_poi:
+	Globals.player_poi -= poison_timer.wait_time
+	if Globals.player_poi > 0:
 		Globals.player_hp -= Globals.poi_dmg
 	else:
-		poison_tick_timer.stop()
-
-func poison_end() -> void:
-	Globals.player_poi = false
-	
+		poison_timer.stop()
--- scenes/world/globals.gd
+++ scenes/world/globals.gd
@@ -1,7 +1,8 @@
 extends Node
 
 var player_hp := 150.0
-var player_poi := false
+var player_poi := 0.0
+var is_player_poi := false
 
-var poi_dmg := 2.5
+var poi_dmg := 1.2
 var on_hit_poi := 1.5
