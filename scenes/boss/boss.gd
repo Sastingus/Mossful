a0fb0be5a283cc921e5f4c0ab310c5568476b89d
TheFlyyingPigs <theflyingpigs10@gmail.com>
2026-07-20 16:30:23 -0500

    new bullet patterns
    -added two new bullet patterns
    -updated spin_pat with another round of bullets
    -made boss randomly pick attacks better

--- scenes/boss/boss.gd
+++ scenes/boss/boss.gd
@@ -2,8 +2,10 @@ extends StaticBody2D
 class_name BossEntity
 
 @export var patterns : Array[PackedScene]
+var previous_patterns := []
 @export var attackSpeed := 5.0
 var attackSpeedRand := 0.5
+
 
 @onready var attack_timer: Timer = $AttackTimer
 
@@ -12,10 +14,20 @@ func _ready() -> void:
 	SignalBus.boss_hit.connect(take_damage)
 
 func attack() -> void:
-	var currentAttack = patterns.pick_random().instantiate()
-	add_child(currentAttack)
-	currentAttack.global_position = Vector2.ZERO
-	attack_timer.wait_time = attackSpeed + randf_range(-attackSpeedRand,attackSpeedRand)
+	var attackPicked := false
+	while not attackPicked:
+		var possibleAttack = patterns.pick_random()
+		if not previous_patterns.has(possibleAttack):
+			previous_patterns.append(possibleAttack)
+			var currentAttack = possibleAttack.instantiate()
+			add_child(currentAttack)
+			currentAttack.global_position = Vector2.ZERO
+			attack_timer.wait_time = attackSpeed + randf_range(-attackSpeedRand,attackSpeedRand)
+			if previous_patterns.size() >= patterns.size():
+				previous_patterns.clear()
+			attackPicked = true
+			print(previous_patterns)
+
 
 func take_damage():
 	Globals.boss_hp -= Globals.player_dmg
@@ -23,4 +35,5 @@ func take_damage():
 		die()
 
 func die():
+	queue_free()
 	print("boss killed!")
